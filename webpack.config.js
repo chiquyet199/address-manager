8d36d68e0e1461065a5315c1875dcd5492ad9a58
Quyet <quyet.nguyen@aleph-labs.com>
2018-03-13 18:43:11 +0700

    Setting up basic react app with simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+module.exports = {
+  entry: ['./src/index'],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve('./'), path.resolve('./node_modules')],
+    extensions: ['.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000
+  }
+}
