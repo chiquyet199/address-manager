0882f3bd156573b5f50bfb0515290a14b56ffdbc
Quyet <quyet.nguyen@aleph-labs.com>
2018-03-13 21:42:17 +0700

    Fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: ['./src/index'],
   module: {
     loaders: [
-      { test: /\.js?$/, loader: 'babel-loader!eslint-loader', exclude: /node_modules/ },
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
     ],
   },
